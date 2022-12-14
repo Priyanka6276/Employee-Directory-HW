ab7be0796c078fb06c4ec43060732e2870289e32
Priyanka6276 <priyankachanbrasegaran@gmail.com>
2022-12-13 23:01:44 -0500

    employee loading

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom"
 
 import EmployeeListItem from "./EmployeeListItem";
-import employeesArr from "../data";
 
 // const employees = employeesArr.map((ele, idx) => {
 //   return <EmployeeListItem key={idx} {...ele} />;
